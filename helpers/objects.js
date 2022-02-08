281c4c546fe59391e98b489cb6f8b5ef70c2cad2
CooperN <norman.cooper.j@gmail.com>
2022-02-07 23:40:43 -0600

    began to write some objects

---
+++ helpers/objects.js
@@ -0,0 +1,26 @@
+
+function createMatchResults() {
+    return {
+        player1:
+        player2:
+        winner:
+        player1crowns:
+        player2crowns:
+    };
+}
+
+function tournamentMatch(weekNumber, player1DiscordName, Player1ClashName, Player2DiscordName, Player2ClashName, League, Winner, Player1Wins, Player2Wins, Player1Towers, Player2Towers) {
+    return {
+        weekNumber,
+        player1DiscordName,
+        Player1ClashName,
+        Player2DiscordName,
+        Player2ClashName,
+        League,
+        Winner,
+        Player1Wins,
+        Player2Wins,
+        Player1Towers,
+        Player2Towers
+    };
+}
